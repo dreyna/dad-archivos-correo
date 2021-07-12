6e25bc1338af7ede63d85891b62bb9acbeccdc92
David Reyna Barreto <reyna_b@upeu.edu.pe>
2021-07-12 01:27:07 -0500

    Update helper.email.js

--- src/libs/helper.email.js
+++ src/libs/helper.email.js
@@ -5,7 +5,7 @@ export const transporter = nodemailer.createTransport({
     secure: true, // true for 465, false for other ports
     auth: {
       user: 'reyna_b@upeu.edu.pe', // generated ethereal user
-      pass: 'jnpthseevbdzpzzc', // generated ethereal password
+      pass: 'jnpthseevbdzpzzc-df', // generated ethereal password
     },
   });
   transporter.verify().then(()=>{
